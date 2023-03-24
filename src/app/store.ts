8385c8909aea6c9c8ec8d8b7bcd228bf17039cf6
rrauf8178 <rrauf8178@gmail.com>
2023-03-25 01:32:18 +0400

    Crated reservationSlice and imported to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import reservationsReducer from "../features/reservationSlice"
 
 export const store = configureStore({
     reducer:{
-
+       reservations: reservationsReducer,
     }
 })
 
